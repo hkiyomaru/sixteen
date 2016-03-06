c04cc2270cd19289e14f1e9a4735bbc1901c52ee
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-06 11:29:37 +0900

    Add routing

--- sixteen/views.py
+++ sixteen/views.py
@@ -4,3 +4,15 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/lesson')
+def lesson():
+    return render_template('lesson.html')
+
+@app.route('/requirement')
+def requirement():
+    return render_template('requirement.html')
